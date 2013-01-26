db75187aa5a85eadaab1d6e7da4b8a04c7004783
Sirius Dely <siriusdely@icloud.com>
2013-01-26 07:03:26 +0700

    before 8 Refactoring

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '311d8804fd30ba5238058b899ba56dfe9b3feaa16ee22996c1ebb4cc0ed9e0b63fb73956351d45767f113ae905170d48d72779110a915151117662a84f3ed3d1'
